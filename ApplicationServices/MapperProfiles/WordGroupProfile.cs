98f0da123f120da9377cb328865e362a3a564848
Jakub T <jakub@tyllo.pl>
2022-12-21 20:38:16 +0100

    Word group editing bug fixed

--- ApplicationServices/MapperProfiles/WordGroupProfile.cs
+++ ApplicationServices/MapperProfiles/WordGroupProfile.cs
@@ -8,6 +8,12 @@ public class WordGroupProfile : Profile
     {
         CreateMap<DAL.Models.WordGroup, ApplicationServices.Domain.Models.WordGroup>();
 
+        CreateMap<ApplicationServices.Domain.Models.WordGroup, DAL.Models.WordGroup>();
+        CreateMap<ApplicationServices.Domain.Models.Word, DAL.Models.Word>()
+        .ForMember(dest => dest.PartOfSpeech, opt => opt.Ignore())
+        .ForMember(dest => dest.AuthorId, opt => opt.MapFrom(src => 1))
+        .ForMember(dest => dest.WordId, opt => opt.MapFrom(src => src.Id));
+
         CreateMap<ApplicationServices.Domain.WordGroupActions.Commands.CreateWordGroupCommand, DAL.Models.WordGroup>()
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.WordGroup!.Name))
             .ForMember(dest => dest.UserADId, opt => opt.MapFrom(src => src.UserADId))
--- DAL/WordGroupRepository.cs
+++ DAL/WordGroupRepository.cs
@@ -13,14 +13,20 @@ public class WordGroupRepository
 
     public async Task<List<WordGroup>> GetAllWordGroups(string userADId)
     {
-        var wordGroups = await _context.WordGroups!.Include(wg => wg.Words).Where(wg => wg.UserADId == userADId).ToListAsync();
+        var wordGroups = await _context.WordGroups!
+            .AsNoTracking()
+            .Include(wg => wg.Words)
+            .Where(wg => wg.UserADId == userADId)
+            .ToListAsync();
         return wordGroups;
 
     }
 
     public async Task<WordGroup> GetWordGroup(int wordGroupId)
     {
-        var wordGroup = await _context.WordGroups!.AsNoTracking().FirstOrDefaultAsync(wg => wg.WordGroupId == wordGroupId);
+        var wordGroup = await _context.WordGroups!
+            .AsNoTracking()
+            .FirstOrDefaultAsync(wg => wg.WordGroupId == wordGroupId);
         return wordGroup!;
     }
 
@@ -31,8 +37,33 @@ public class WordGroupRepository
     }
     public async Task EditWordGroup(WordGroup wordGroup)
     {
-        _context.Update(wordGroup);
-        await _context.SaveChangesAsync();
+        using (var transaction = _context.Database.BeginTransaction())
+        {
+
+            var wordGroupFromDB = _context.WordGroups!
+                .Include(wg => wg.Words)
+                .FirstOrDefault(wg => wg.WordGroupId == wordGroup.WordGroupId);
+
+            _context.Entry(wordGroupFromDB!).CurrentValues.SetValues(wordGroup);
+
+            foreach (var word in wordGroup.Words)
+            {
+                var wordFromDB = wordGroupFromDB.Words
+                    .FirstOrDefault(w => w.WordId == word.WordId);
+                if (wordFromDB == null) wordGroupFromDB.Words.Add(word);
+                else _context.Entry(wordFromDB).CurrentValues.SetValues(word);
+            }
+            _context.SaveChanges();
+
+            foreach (var word in wordGroupFromDB.Words)
+            {
+                if (!wordGroup.Words.Any(w => w.WordId == word.WordId))
+                {
+                    _context.Database.ExecuteSqlInterpolated($"DELETE FROM dbo.WordWordGroup WHERE WordsWordId = {word.WordId} AND WordGroupsWordGroupId = {wordGroupFromDB.WordGroupId}");
+                }
+            }
+            await transaction.CommitAsync();
+        }
     }
 
     public async Task DeleteWordGroup(int wordGroupId)
@@ -45,6 +76,7 @@ public class WordGroupRepository
     public async Task<bool> CheckIfWordGroupExists(string userADId, string wordGroupName)
     {
         var wordGroup = await _context.WordGroups!
+                    .AsNoTracking()
                     .SingleOrDefaultAsync(wg => wg.Name == wordGroupName && wg.UserADId == userADId);
         if (wordGroup == default) return false;
         return true;
@@ -53,6 +85,7 @@ public class WordGroupRepository
     public async Task<bool> CheckIfUserIsAuthorized(string userADId, int wordGroupId)
     {
         var wordGroup = await _context.WordGroups!
+                    .AsNoTracking()
                     .SingleOrDefaultAsync(wg => wg.WordGroupId == wordGroupId && wg.UserADId == userADId);
         if (wordGroup == default) return false;
         return true;
